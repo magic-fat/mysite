99f50dd29ea117ba81d660d99c51067cad458028
magic-fat <e.grebennik@mail.ru>
2016-08-31 23:06:50 +0300

    uuuuf

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'welcome#index'
   resources :users
-  get 'register' , to: 'user#new' , as: 'register'
+  get '/register' , to: 'user#new' , as: '/register'
 end
