e8d4990d65630fe2bb13e1c3e846f6f8488dcbf3
magic-fat <e.grebennik@mail.ru>
2016-09-02 20:34:29 +0300

    uuuuuf

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,8 @@
+class UserMailer < ApplicationMailer
+	default from: 't3atime@rambler.ru'
+	def welcome_email(user)
+		#@user = user;
+		@url = 'http://t3atime.herokuapp.com/verify'
+		mail(to: 'e.grebennik@mail.ru' , subject:'TEST')
+	end
+end
