cf86cb6c22eaec8760b4a47ffb2e3af851e4e816
magic-fat <e.grebennik@mail.ru>
2016-09-02 23:46:37 +0300

    uuuuf

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
 
 	def create
 		@user = First.new(params.require(:register).permit(:email))
+		@user.save
 		session[:test] = 'test'
 		UsersMailer.welcome_email(@user).deliver_later
 		redirect_to '/register'
