90eecc9f5f639a13003f8585cfdb5394f0ff1b04
magic-fat <e.grebennik@mail.ru>
2016-09-02 23:37:49 +0300

    uuuuf

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,9 @@ class UsersController < ApplicationController
 	end
 
 	def create
+		@user = First.new(params.require(:register).permit(:email))
 		session[:test] = 'test'
-		UsersMailer.welcome_email('e.grebennik@rambler.ru')
+		UsersMailer.welcome_email(@user)
 		redirect_to '/register'
 	end
 end
--- app/mailers/users_mailer.rb
+++ app/mailers/users_mailer.rb
@@ -1,8 +1,8 @@
 class UsersMailer < ApplicationMailer
 	default from: 't3atime@rambler.ru'
 	def welcome_email(user)
-		#@user = user;
+		@user = user;
 		@url = 'http://t3atime.herokuapp.com/verify'
-		mail(to: 'e.grebennik@mail.ru' , subject:'TEST')
+		mail(to: @user.email , subject:'TEST')
 	end
 end
