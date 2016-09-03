6b28186cbf5905ccea43740132f15151636ef9c6
magic-fat <e.grebennik@mail.ru>
2016-09-03 19:27:27 +0300

    uuuuf

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,7 +26,16 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.delivery_method = :smpt
+  ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
+}
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
 
